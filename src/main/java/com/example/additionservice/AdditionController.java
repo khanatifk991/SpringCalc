0e3671e5b9f53b579d7f91c33fa9416224546ff7
Rutika <2000rutika@gmail.com>
2024-03-10 20:01:11 +0530

    basic calculator

--- src/main/java/com/example/additionservice/AdditionController.java
+++ src/main/java/com/example/additionservice/AdditionController.java
@@ -1,8 +1,6 @@
 package com.example.additionservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AdditionController {
@@ -12,4 +10,35 @@ public class AdditionController {
         int sum = a + b;
         return "The sum of " + a + " and " + b + " is " + sum;
     }
+
+    @PostMapping("/subtract")
+    public String subtractNumbers(@RequestBody NumbersRequest numbersRequest) {
+        int result = numbersRequest.getFirstNumber() - numbersRequest.getSecondNumber();
+        return "The result of subtracting " + numbersRequest.getSecondNumber() + " from " +
+                numbersRequest.getFirstNumber() + " is " + result;
+    }
+
+    @PostMapping("/multiply")
+    public String multiplyNumbers(@RequestBody NumbersRequest numbersRequest) {
+        int result = numbersRequest.getFirstNumber() * numbersRequest.getSecondNumber();
+        return "The result of multiplying " + numbersRequest.getFirstNumber() + " and " +
+                numbersRequest.getSecondNumber() + " is " + result;
+    }
+
+    @PostMapping("/divide")
+    public String divideNumbers(@RequestBody NumbersRequest numbersRequest) {
+        if (numbersRequest.getSecondNumber() == 0) {
+            return "Error: Division by zero is not allowed";
+        }
+        double result = (double) numbersRequest.getFirstNumber() / numbersRequest.getSecondNumber();
+        return "The result of dividing " + numbersRequest.getFirstNumber() + " by " +
+                numbersRequest.getSecondNumber() + " is " + result;
+    }
+    @PutMapping("/updateFirstNumber/{value}")
+    public String updateFirstNumber(@PathVariable int value) {
+         firstNumber.setfirstNumber(value);
+        return "Number first updated to " + value;
+    }
+
+
 }
---
+++ src/main/java/com/example/additionservice/firstNumber.java
@@ -0,0 +1,17 @@
+package com.example.additionservice;
+
+public class firstNumber {
+
+        public static int firstnumber;
+
+
+        public static int getfirstNumber() {
+            return firstnumber;
+        }
+
+        public static void setfirstNumber(int value) {
+            firstnumber = value;
+        }
+
+
+}
